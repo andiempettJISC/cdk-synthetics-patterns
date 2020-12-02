4c936077bfee8255a2c8138d0c4891e190371988
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2020-12-02 22:21:06 +0000

    basic lib and tests

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,54 @@
 import * as cdk from '@aws-cdk/core';
+import * as synthetics from '@aws-cdk/aws-synthetics';
+import {Alarm, ComparisonOperator} from '@aws-cdk/aws-cloudwatch';
+import { SnsAction } from '@aws-cdk/aws-cloudwatch-actions';
+import { Topic } from '@aws-cdk/aws-sns';
+import { EmailSubscription } from '@aws-cdk/aws-sns-subscriptions';
+import path = require('path');
 
 export interface SyntheticsPatternsProps {
-  // Define construct properties here
+  /**
+   * The amount of time in minutes the synthetic should check
+   */
+  checkInterval?: number,
+
+  /**
+   * The email address to send Fail notifications to
+   */
+  email: string
 }
 
 export class SyntheticsPatterns extends cdk.Construct {
 
-  constructor(scope: cdk.Construct, id: string, props: SyntheticsPatternsProps = {}) {
+  constructor(scope: cdk.Construct, id: string, props: SyntheticsPatternsProps) {
     super(scope, id);
 
-    // Define construct contents here
+    let checkInterval: number
+    if (props.checkInterval === undefined) {
+      checkInterval = 5
+    } else {
+      checkInterval = props.checkInterval
+    }
+
+    const canary = new synthetics.Canary(this, 'SimpleCanary', {
+      schedule: synthetics.Schedule.rate(cdk.Duration.minutes(checkInterval)),
+      test: synthetics.Test.custom({
+        code: synthetics.Code.fromAsset(path.join(__dirname, '../', 'canary')),
+        handler: 'index.handler',
+      }),
+      runtime: synthetics.Runtime.SYNTHETICS_NODEJS_2_0,
+    });
+
+    const CanaryTopic = new Topic(this, 'CanaryTopic')
+
+    CanaryTopic.addSubscription(new EmailSubscription(props.email))
+
+    new Alarm(this, 'CanaryAlarm', {
+      metric: canary.metricSuccessPercent(),
+      evaluationPeriods: 2,
+      threshold: 90,
+      comparisonOperator: ComparisonOperator.LESS_THAN_THRESHOLD,
+    }).addAlarmAction(new SnsAction(CanaryTopic));
+
   }
 }
--- test/synthetics-patterns.test.ts
+++ test/synthetics-patterns.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, countResources } from '@aws-cdk/assert';
+import { expect as expectCDK, countResources, exactlyMatchTemplate } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SyntheticsPatterns from '../lib/index';
 
@@ -9,7 +9,7 @@ test('SNS Topic Created', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
   // WHEN
-  new SyntheticsPatterns.SyntheticsPatterns(stack, 'MyTestConstruct');
+  new SyntheticsPatterns.SyntheticsPatterns(stack, 'MyTestConstruct', {email: 'example@example.com'});
   // THEN
-  expectCDK(stack).to(countResources("AWS::SNS::Topic",0));
+  expectCDK(stack).to(countResources("AWS::SNS::Topic",1));
 });
