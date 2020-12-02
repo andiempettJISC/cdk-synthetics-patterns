994af90190757791c4fe1819f8faaebb858bd410
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2020-12-02 21:31:37 +0000

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface SyntheticsPatternsProps {
+  // Define construct properties here
+}
+
+export class SyntheticsPatterns extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: SyntheticsPatternsProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
